8c0415483dfb5cb4ee5449405bf29d94ba22c20a
Pulkit <pulkitmlk1802@gmail.com>
2023-09-06 23:32:38 +0530

    :tada: prisma update and delete functions

--- src/utils/service.utils.ts
+++ src/utils/service.utils.ts
@@ -67,3 +67,75 @@ export const prismaFindMany = async (
         return res;
 
 }
+
+export const prismaUpdate = async (
+    model: keyof PrismaClient,
+    where: any,
+    data: any,
+    options: any
+    ) => {
+
+        // @ts-ignore
+        const res = await prisma[model].update({
+            where,
+            data,
+            ...options
+        })
+
+        return res;
+
+}
+
+export const prismaUpdateMany = async (
+    model: keyof PrismaClient,
+    where: any,
+    data: any,
+    options: any
+    ) => {
+            
+            // @ts-ignore
+            const res = await prisma[model].updateMany({
+                where,
+                data,
+                ...options
+            })
+    
+            return res;
+
+}
+
+export const prismaDelete = async (
+    model: keyof PrismaClient,
+    where: any,
+    options: any
+    ) => {
+
+        // @ts-ignore
+        const res = await prisma[model].delete({
+            where,
+            ...options
+        })
+
+        return {
+            status: "deleted",
+        };
+
+}
+
+export const prismaDeleteMany = async (
+    model: keyof PrismaClient,
+    where: any,
+    options: any
+    ) => {
+
+        // @ts-ignore
+        const res = await prisma[model].deleteMany({
+            where,
+            ...options
+        })
+
+        return {
+            status: "deleted",
+        };
+
+}
