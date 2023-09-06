f3ae0c85eb72787ce50359b080ab5c46ca944ccf
Pulkit <pulkitmlk1802@gmail.com>
2023-09-06 23:07:26 +0530

    :sparkles: Added logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+import configs from '../config/config';
+
+const enumerateErrorFormat = winston.format((info) => {
+    if (info instanceof Error) {
+        Object.assign(info, { message: info.stack });
+    }
+    return info;
+});
+
+const logger = winston.createLogger({
+    level: configs.env === 'dev' ? 'debug' : 'info',
+    format: winston.format.combine(
+    enumerateErrorFormat(),
+    configs.env === 'dev'
+        ? winston.format.colorize()
+        : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+    ),
+    transports: [
+        new winston.transports.Console({
+        stderrLevels: ['error'],
+        }),
+    ],
+})
+
+export default logger;
