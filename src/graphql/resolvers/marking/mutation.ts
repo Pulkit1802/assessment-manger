ee78686d67416f7e3b08f7925f248765fec42422
Pulkit <pulkitmlk1802@gmail.com>
2023-10-19 03:46:48 +0530

    :zap: markings

---
+++ src/graphql/resolvers/marking/mutation.ts
@@ -0,0 +1,74 @@
+import { prisma } from "../../../config";
+import ApiError from "../../../utils/apiError";
+
+export const mutations = {
+    createMarking: async (_: any, args: any) => {
+        const {data} = args || {};
+        const {questionMarks} = data;
+
+        const testDetails = await prisma.test.findUnique({
+            where: {
+                id: data.testId,
+            },
+            include: {
+                parts: {
+                    include: {
+                        questions: true,
+                    }
+                },
+            },
+        });
+
+        if (!testDetails)
+            throw new ApiError(404, "Test not found");
+
+        const {parts} = testDetails;
+        
+        parts.forEach(async (part: any) => {
+            const {questions} = part;
+            const partQuestionMarks = questionMarks.filter((questionMark: any) => questionMark.partId == part.id);
+            if (partQuestionMarks.length < part.requiredQuestions) {
+
+                const diff = part.requiredQuestions - partQuestionMarks.length;
+                const unmarkedQuestion = questions.filter((question: any) => {
+                    const questionMark = partQuestionMarks.find((questionMark: any) => questionMark.questionId == question.id);
+                    return !questionMark;
+                });
+
+                for(let i=0; i < diff; i++) {
+                    questionMarks.push({
+                        questionId: unmarkedQuestion[i].id,
+                        partId: part.id,
+                        marksObtained: 0,
+                    });
+                }
+
+            }
+        });
+
+        // @ts-ignore
+        delete data.questionMarks;
+
+        const totalMarks = questionMarks.reduce((acc: any, questionMark: any) => acc + questionMark.marksObtained, 0);
+
+        console.log(questionMarks);
+
+        const marking = await prisma.marking.create({
+            data:{
+                ...data,
+                totalMarksObtained: totalMarks,
+                questionWiseMarksObtained: {
+                    createMany: {
+                        data: questionMarks,
+                    },
+                },
+            },
+            include: {
+                questionWiseMarksObtained: true,
+            }
+        });
+
+        return marking
+
+    }
+};
