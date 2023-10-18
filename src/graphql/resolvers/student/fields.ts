8cbe007da58c0e1ed3222560066164ea106f3a3d
Pulkit <pulkitmlk1802@gmail.com>
2023-10-19 01:25:20 +0530

    :zap: student fields

---
+++ src/graphql/resolvers/student/fields.ts
@@ -0,0 +1,59 @@
+import { prisma } from "../../../config";
+
+const fields = {
+    Student: {
+        sections: async (parent: any, _: any) => {
+            return await prisma.section.findMany({
+                where: {
+                    students: {
+                        some: {
+                            id: parent.id
+                        }
+                    }
+                },
+            });
+        },
+        fa: async (parent: any, _: any) => {
+            return await prisma.user.findFirst({
+                where: {
+                    id: parent.faId
+                }
+            });
+        },
+        dept: async (parent: any, _: any) => {
+            return await prisma.dept.findFirst({
+                where: {
+                    id: parent.deptId
+                }
+            });
+        },
+        courses: async (parent: any, _: any) => {
+            return await prisma.section.findMany({
+                where: {
+                    students: {
+                        some: {
+                            id: parent.id
+                        }
+                    }
+                },
+                select: {
+                    course: true
+                }
+            });
+        },
+        teachers: async (parent: any, _: any) => {
+            return await prisma.section.findMany({
+                where: {
+                    students: {
+                        some: {
+                            id: parent.id
+                        }
+                    }
+                },
+                select: {
+                    faculty: true
+                }
+            });
+        }
+    }
+}
