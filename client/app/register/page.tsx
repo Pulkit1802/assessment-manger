17951299213a4cd12be79f8a8a29433de5f0bd1a
KartikeyCode <codekartikey@gmail.com>
2024-03-06 21:14:38 +0530

    fix register edit sections

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,8 +1,10 @@
 import { RegisterForm } from "../components/form/registerForm";
-
+import Image from "next/image";
+import uni from "@/public/university.png"
 export default function Page () {
     return (
-        <div className="h-screen bg-gray-200 flex justify-center items-center">
+        <div className="h-screen bg-gray-200 p-14 flex 2xl:gap-28 flex-col sm:flex-row justify-center items-center">
+            <Image src={uni} alt="university" className="2xl:w-[55rem] hidden sm:block " />
             <RegisterForm />
         </div>
     )
